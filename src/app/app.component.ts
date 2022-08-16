7d6eef7436291a5f3b683004d9ed76fc65652c04
Sasha <sashakostina@yahoo.com>
2022-08-16 11:51:27 +0200

    creation d'application, creation d'une classe intern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+//decorateur - design pattern, sert ajouter dynamiquent le comportement a une classe
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  //Attributs de classe
+  private title = 'Hello Angular 13!';
+
+  public intern : any = {
+    name: 'Aubert',
+    firstname: 'Jean-Luc',
+    phoneNumber: '(+33)6 23 25 58 87',
+    email: 'jla.webprojet@gmail.com'
+  }
+
+  public getTitle() : string {
+    return this.title;
+  }
 }
---
+++ src/app/core/models/intern.ts
@@ -0,0 +1,9 @@
+export class Intern {
+  public id?: number; //? optionnel
+  public name!: string;  //! obligatoire
+  public firstname!: string;
+  public phoneNumber?: string;
+  public email?: string;
+  public birthDate?: Date;
+  public address?: string;
+}
