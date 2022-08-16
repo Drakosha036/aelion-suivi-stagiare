ecc336a9d1a0e5a91b1a1227ab261ffcf5729f13
Sasha <sashakostina@yahoo.com>
2022-08-16 14:57:31 +0200

    creation de tableau des interns et l'affichage dans template grace au directive *ngFor - fait boucle for pour parcourir le tableau

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,35 @@ export class AppComponent {
   //Attributs de classe
   private title = 'Hello Angular 13!';
 
+  //objet intern qui est cree a partir de la classe Intern
   public intern : Intern = {
     name: 'Aubert',
     firstname: 'Jean-Luc',
     phoneNumber: '(+33)6 23 25 58 87',
     email: 'jla.webprojet@gmail.com'
   }
+
+  public interns: Intern[] = [
+    this.intern,
+    {
+      name: 'Pina',
+      firstname: 'Monica',
+      phoneNumber: '(+33)6 23 25 58 87',
+      email: 'monica@gmail.com'
+    },
+    {
+      name: 'Kostina',
+      firstname: 'Sasha',
+      phoneNumber: '(+33)6 23 25 58 87',
+      email: 'sasha@gmail.com'
+    },
+    {
+      name: 'Marchand',
+      firstname: 'Glace',
+      phoneNumber: '(+33)6 23 25 58 87',
+      email: 'glace@gmail.com'
+    }
+  ]
 
   public getTitle() : string {
     return this.title;
